f2cad48f93eb4332f4ccce6c8a2f6ee557342a25
Logan <loganmsorensen@gmail.com>
2021-08-07 18:59:30 -0700

    created site structure

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import Button from "../../styles/GlobalComponents/Button";
+import { LeftSection } from "./HeroStyles";
+
+const Hero = (props) => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Hi, I'm Logan Sorensen <br />
+        I'm a Fullstack Web Developer
+      </SectionTitle>
+      <SectionText>Value propostion</SectionText>
+      <Button
+        onClick={() => (window.location = "http://localhost:3000#projects")}
+      >
+        Check out my work
+      </Button>
+    </LeftSection>
+  </Section>
+);
+
+export default Hero;
